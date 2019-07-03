57d360e82ad72cc825bb91d722dfedd569d57aa6
Dipak9823 <dipak363@gmail.com>
2019-07-03 19:32:19 +0530

    Note Add frontend completed

--- client/FundooApp/src/app/Component/notes/notes.component.ts
+++ client/FundooApp/src/app/Component/notes/notes.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder, FormGroup, Validator } from '@angular/forms';
+import { RootService} from '../../root.service';
+import { NoteModel } from "../../model/notemodel";
 @Component({
   selector: 'app-notes',
   templateUrl: './notes.component.html',
@@ -7,12 +9,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NotesComponent implements OnInit {
 popUp: boolean;
-  constructor() { }
+addNote:FormGroup;
+noteModel:NoteModel=new NoteModel();
+
+  constructor(
+    private formbuilder:FormBuilder,
+    private rootservice:RootService
+  ) { }
 
   ngOnInit() {
+    
+
   }
 
   popOut(){
     this.popUp=true;
+  }
+  onSubmit() {
+   this.popUp=!this.popUp;
+   // console.log("The Data is:-",this.addNote.value);
+   console.log("Note is created"+this.noteModel.title)
+     
+   console.log("Note is created"+this.noteModel.description)
+
   }
 }
